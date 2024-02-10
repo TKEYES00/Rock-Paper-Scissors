f21efbc88c991211802535d99a976f06c11c58d1
Tray Keyes <trayton26@gmail.com>
2024-02-09 23:19:15 -0800

    additional CSS and Js added

---
+++ main.js
@@ -0,0 +1,19 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const results = document.getElementById("resultsText");
+const userScore = document.getElementById("userScore");
+const compScore = document.getElementById("compScore");
+
+let randomNumber = 0;
+function getRandNumber() {
+    return (
+       randomNumber =  Math.floor(Math.random() * 3)
+    )
+}
+
+function chooseWinner() {
+    return(
+null
+    )
+}
